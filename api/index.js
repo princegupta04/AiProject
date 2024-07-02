5b8284901e9d7d47a0d0a5ec1a4acc31dd8de563
PRINCE GUPTA <98451406+princegupta04@users.noreply.github.com>
2024-07-02 12:23:44 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ import authRouter from './routes/auth.route.js';
 import listingRouter from './routes/listing.route.js';
 import cookieParser from 'cookie-parser';
 import path from 'path';
-require("dotenv").config();
+dotenv.config();
 
 
 
